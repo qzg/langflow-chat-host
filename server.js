99ebc91483a37a75ad63f70d76e4d3ca9513269b
Kiyu Gabriel <q@qzg.us>
2024-05-07 04:15:13 -0500

    generalizing the code

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
 
 app.use(express.static('public'))
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Example app listening on port ${port}`)
 })
